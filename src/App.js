3ba78e148d26e3835c69bc43fc933e78c8484af5
Luiz <luizmitsuru@gmail.com>
2020-09-03 23:20:19 -0300

    Finalizado

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import "./App.css";
 import Tmdb from "./Tmdb";
 import MovieRow from "./components/MovieRow";
 import FeaturedMovie from "./components/FeaturedMovie";
+import Header from "./components/Header";
 
 export default () => {
   const [movieList, setMovieList] = useState([]);
   const [featuredData, setFeaturedData] = useState(null);
+  const [blackHeader, setBlackHeader] = useState(false);
 
   useEffect(() => {
     const loadAll = async () => {
@@ -27,8 +29,25 @@ export default () => {
     loadAll();
   }, []);
 
+  useEffect(() => {
+    const scrollListener = () => {
+      if (window.scrollY > 10) {
+        setBlackHeader(true);
+      } else {
+        setBlackHeader(false);
+      }
+    };
+    window.addEventListener("scroll", scrollListener);
+
+    return () => {
+      window.removeEventListener("scroll", scrollListener);
+    };
+  }, []);
+
   return (
     <div className="page">
+      <Header black={blackHeader} />
+
       {featuredData && <FeaturedMovie item={featuredData} />}
 
       <section className="lists">
@@ -36,6 +55,24 @@ export default () => {
           <MovieRow key={key} title={item.title} items={item.items} />
         ))}
       </section>
+      <footer>
+        Feito com{" "}
+        <span role="img" aria-label="coração">
+          ❤
+        </span>{" "}
+        por Luiz Mitsuru Dai, todos os direitos de imagens são da Netflix. Dados
+        Extraídos do site https://www.themoviedb.org/. Agradecimento especial
+        para Bonieky Lacerda do curso https://b7web.com.br/fullstack por ensinar
+        a criar este site.
+      </footer>
+      {movieList.length <= 0 && (
+        <div className="loading">
+          <img
+            src="https://cdn.lowgif.com/small/0534e2a412eeb281-the-counterintuitive-tech-behind-netflix-s-worldwide.gif"
+            alt="Carregando"
+          />
+        </div>
+      )}
     </div>
   );
 };
